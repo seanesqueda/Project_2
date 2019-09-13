4ab7041926be67b0179f18ed43842e7d63dbc029
Austin Kang <austinkang14@gmail.com>
2019-09-12 23:49:22 -0700

    profile html

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -15,6 +15,10 @@ module.exports = function(app) {
     res.sendFile(path.join(__dirname, "../views/register.html"));
   });
 
+  app.get("/profile", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/profile.html"));
+  });
+
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
     res.render("404");
